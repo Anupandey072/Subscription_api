5124c8973e571d8e35e04794e38f04153c0732ab
kdhoni07 <kdhoni07@icloud.com>
2024-04-18 17:22:19 +0530

    commit

--- obj/Debug/net6.0/subscription_api.AssemblyInfo.cs
+++ obj/Debug/net6.0/subscription_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("subscription_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ec5df7b637784912538a93c132e6e15002c9b44")]
 [assembly: System.Reflection.AssemblyProductAttribute("subscription_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("subscription_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
